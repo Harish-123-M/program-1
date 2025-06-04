4920f340b319654be9bbd2b0a8317371fe7c1d5e
Harish-123-M <maruboinaharish620@gmail.com>
2025-06-04 10:51:09 +0530

    Add files via upload

---
+++ arithmetic.2.py
@@ -0,0 +1,17 @@
+a=eval(input("enter first value"))
+
+b=eval(input("enter second value"))
+
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+print(a%b)
+
+print(a//b)
+
+print(a**b)
---
+++ circle.1.py
@@ -0,0 +1,2 @@
+D=eval(input("enter dia-meter of circle:"))
+print("the radius of circle is : ",D/2)
